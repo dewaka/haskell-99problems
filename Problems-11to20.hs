05dc31152acbe6343fa53ecc52e663651ec189b5
dewaka <dcdewaka@gmail.com>
2013-01-12 01:45:22 +0000

    Problems 14 to 20 solved.

--- Problems-11to20.hs
+++ Problems-11to20.hs
@@ -21,3 +21,49 @@ encodeModified x = map (\(n, x) -> if n == 1 then (Single x) else (Multiple n x)
 
 -- 12) Decode a run-length encoded list.
 
+decodeModified :: [Encoding a Int] -> [a]
+decodeModified = loop []
+    where loop acc [] = reverse acc
+          loop acc (x:xs) = case x of
+                              (Single a) -> loop (a : acc) xs
+                              (Multiple n a) -> loop ((take n (repeat a)) ++ acc) xs
+              
+skipWhile :: (a -> Bool) -> [a] -> [a]
+skipWhile p [] = []              
+skipWhile p ls@(x:xs) = if not (p x)
+                     then ls
+                     else skipWhile p xs
+
+breakDown :: Eq a => [a] -> [[a]]
+breakDown = loop []
+    where loop acc [] = reverse acc
+          loop acc ls@(x:xs) = loop ((takeWhile (== x) ls) : acc) (skipWhile (== x) ls)
+
+-- 14) Duplicate the elements of a list.
+dupli xs = foldr (++) [] [replicate 2 x | x <- xs]
+
+-- 15) Replicate the elements of a list a given number of times.
+repli xs n = foldr (++) [] [replicate n x | x <- xs]
+
+-- 16)  Drop every N'th element from a list.
+dropEvery [] _ = []
+dropEvery xs n = take (n-1) xs ++ dropEvery (drop n xs) n
+
+-- 17) Split a list into two parts; the length of the first part is given.
+-- split "abcdefghik" 3 -> ("abc", "defghik")
+split xs n = loop [] xs n
+    where loop acc xs 0 = [acc, xs]
+          loop acc (x:xs) n = loop (acc ++ [x]) xs (n-1)
+
+-- 18) Extract a slice from a list.
+slice xs m n = take (n - m + 1) . drop (m - 1) $ xs
+
+-- 19) Rotate a list N places to the left.
+rotate xs n
+       | n >= 0 = drop n xs ++ take n xs
+       | otherwise = rotate xs ((length xs) + n)
+
+-- 20) Remove the K'th element from a list.
+removeAt n xs = loop n xs []
+    where loop 1 (x:xs) acc = (x, acc ++ xs)
+          loop n (x:xs) acc = loop (n-1) xs (acc ++ [x])
